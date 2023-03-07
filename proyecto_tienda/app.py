2e3007b43c4ebb12034b3767c9c14c2c67e02376
JesusYael <1721110698@utectulancingo.edu.mx>
2023-03-07 23:52:53 +0000

    tabla productos

--- proyecto_tienda/app.py
+++ proyecto_tienda/app.py
@@ -1,8 +1,12 @@
 import web
-
+from web import form
 urls = (
     '/', 'mvc.controllers.bienvenida.index.Index',
-    '/productos', 'mvc.controllers.productos.productos.mProductos'
+    '/productos', 'mvc.controllers.productos.ver_productos.mostrar_Productos',
+    '/agregar_productos', 'mvc.controllers.productos.insertar_productos.agregar_productos',
+    '/eliminar_producto/(\d+)', 'mvc.controllers.productos.eliminar_productos.eliminar_productos',
+    '/editar_producto/(\d+)', 'mvc.controllers.productos.editar_producto.editar_productos',
+    
 )
 app = web.application(urls, globals()) #aplicacion web
 
---
+++ proyecto_tienda/mvc/controllers/productos/ver_productos.py
@@ -0,0 +1,27 @@
+import web
+import sqlite3
+
+render = web.template.render('proyecto_tienda/mvc/views')
+
+with sqlite3.connect("proyecto_tienda/db/despensa.sqlite3") as connection:
+    #connection.row_factory: sqlite3.Row
+    cursor = connection.cursor()
+    sql = ("select * from productos")
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+regresar= web.url('/productos')
+
+class mostrar_Productos:
+    def GET(self):
+        agregar= web.url('/agregar_productos')
+        with sqlite3.connect("proyecto_tienda/db/despensa.sqlite3") as connection:
+            #connection.row_factory: sqlite3.Row
+            cursor = connection.cursor()
+            sql = ("select * from productos")
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+        return render.productos.lista(result,agregar)
+        
+        
