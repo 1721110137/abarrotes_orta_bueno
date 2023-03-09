bf98fd9856b289bd184a7ec4ca122263dd1de8e2
JavierToriz <97756451+JavierToriz@users.noreply.github.com>
2023-03-08 20:58:42 -0600

    Update app.py

--- proyecto_tienda/app.py
+++ proyecto_tienda/app.py
@@ -6,6 +6,10 @@ urls = (
     '/agregar_productos', 'mvc.controllers.productos.insertar_productos.agregar_productos',
     '/eliminar_producto/(\d+)', 'mvc.controllers.productos.eliminar_productos.eliminar_productos',
     '/editar_producto/(\d+)', 'mvc.controllers.productos.editar_producto.editar_productos',
+    '/despensa', 'mvc.controllers.despensa.ver_despensa.mostrar_Despensa',
+    '/agregar_despensa', 'mvc.controllers.despensa.insertar_despensa.agregar_despensa',
+    '/editar_despensa/(\d+)', 'mvc.controllers.despensa.editar_despensa.editar_despensa',
+    '/eliminar_despensa/(\d+)', 'mvc.controllers.despensa.eliminar_despensa.eliminar_despensa',
     
 )
 app = web.application(urls, globals()) #aplicacion web
